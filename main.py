262d6c43ee521425213ae06f1f0ba66c4b967c5c
RaphNox <128356695+RaphNox@users.noreply.github.com>
2023-05-24 14:47:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ from functions import *
 def menu():
     print("\n==== Programming Quotes ====")
     print("1. Random quote")
-    print("2. All quotes")
-    print("3. Add a quote")
-    print("4. Exit")
+    print("2. Any quote")
+    print("3. Exit")
 
 def main():
     while True:
@@ -13,14 +12,13 @@ def main():
         menu()
 
         choice = input("Choose your an action (1-3): ")
-        
+
         if choice == "1":
             print_quote(random_quote(quotes))
         elif choice == "2":
-            view_quotes(quotes)
+            count = int(input("Enter the number of quotes to display: "))
+            display_quotes(quotes, count)
         elif choice == "3":
-            add_quote(quotes, "quotes.txt")
-        elif choice == "4":
             print("Good bye...")
             break
         else:
